865801a1083dacf12476bbb90e26b6e477d4e9f6
andrew-gh <andrew-gh@users.noreply.github.com>
2016-06-24 23:40:24 -0400

    Multistep test

--- core/src/test/scala/org/scalabatch/FlatFileJobTest.scala
+++ core/src/test/scala/org/scalabatch/FlatFileJobTest.scala
@@ -20,4 +20,23 @@ class FlatFileJobTest extends FunSuite {
       new File(output).delete()
     }
 
+    test("FlatFileMultiStepJobTest"){
+      val input = getClass.getResource("/input1.txt").getPath
+      val output = input.replace("input1.txt", "output1.txt")
+      val output2 = input.replace("input1.txt", "output2.txt")
+      new JobBuilder("1").addStep("1").addTask("1","FlatFileReadTask", input).
+        addTask("2","FlatFileWriteTask", output).
+        addStep("2").addTask("3","FlatFileReadTask", input).
+        addTask("4","FlatFileWriteTask", output2).build().executeJob()
+      assert(new File(output).exists(), "Output file cannot be read")
+      assert(new File(output2).exists(), "Output file 2 cannot be read")
+      var writtenContent = Source.fromFile(output).getLines().mkString
+      assert(writtenContent.equals("Line 1Line 2"), "Content of output file differs from input file")
+      new File(output).delete()
+      writtenContent = Source.fromFile(output2).getLines().mkString
+      assert(writtenContent.equals("Line 1Line 2"), "Content of output file differs from input file")
+      new File(output2).delete()
+    }
+
+
 }
