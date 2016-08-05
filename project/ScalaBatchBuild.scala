37844223ad04d49e60df1ee9da0befda43fb30d1
andrew-gh <andrew-gh@users.noreply.github.com>
2016-08-05 01:47:36 -0400

    Sonatype publishing

--- project/ScalaBatchBuild.scala
+++ project/ScalaBatchBuild.scala
@@ -64,4 +64,16 @@ object ScalaBatchBuild extends Build {
       }
     )
   ) dependsOn(core, rest)
+
+  (for {
+    username <- Option(System.getenv().get("SONATYPE_USERNAME"))
+    password <- Option(System.getenv().get("SONATYPE_PASSWORD"))
+  } yield
+    credentials += Credentials(
+      "Sonatype Nexus Repository Manager",
+      "oss.sonatype.org",
+      username,
+      password)
+    ).getOrElse(credentials ++= Seq())
+
 }
